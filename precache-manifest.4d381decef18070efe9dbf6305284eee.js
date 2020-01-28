41d9987400885410af2353b809b5259bb9fafb96
TipaMag <perynsky@gmail.com>
2020-01-28 05:56:47 +0200

    Updates

---
+++ precache-manifest.4d381decef18070efe9dbf6305284eee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7071259de941902354caf322d44a5061",
+    "url": "//tipamag.github.io/test-blog-react-app/index.html"
+  },
+  {
+    "revision": "fffe7d35b63a0cf6c29e",
+    "url": "//tipamag.github.io/test-blog-react-app/static/css/main.34ffbfea.chunk.css"
+  },
+  {
+    "revision": "90a9691b5e59831453e4",
+    "url": "//tipamag.github.io/test-blog-react-app/static/js/2.8106608f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "//tipamag.github.io/test-blog-react-app/static/js/2.8106608f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fffe7d35b63a0cf6c29e",
+    "url": "//tipamag.github.io/test-blog-react-app/static/js/main.27fe0d3d.chunk.js"
+  },
+  {
+    "revision": "2a71ca57c88832cf5edb",
+    "url": "//tipamag.github.io/test-blog-react-app/static/js/runtime-main.7dbfd010.js"
+  },
+  {
+    "revision": "5682ea5ffeca5010cc8a35fabac12d49",
+    "url": "//tipamag.github.io/test-blog-react-app/static/media/404-not-found.5682ea5f.jpg"
+  },
+  {
+    "revision": "f6076e2ce66330bfb7e76ffb26986dad",
+    "url": "//tipamag.github.io/test-blog-react-app/static/media/Spinner-preloader.f6076e2c.svg"
+  }
+]);
